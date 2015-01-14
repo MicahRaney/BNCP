15a7bebe3d98ce2080648be8695e7697026ffe1a
Micah Raney <MicahRaney@users.noreply.github.com>
2015-01-14 19:59:14 +0530

    Removed redundant constructor parameter device. The device will obviously be a motor!

--- BNCP PC/src/bncp/pc/io/MotorPacket.java
+++ BNCP PC/src/bncp/pc/io/MotorPacket.java
@@ -6,7 +6,7 @@ import java.io.DataOutputStream;
 
 public class MotorPacket extends Packet {
 
-	public int device, port, value;
+	public int port, value;
 	public boolean forward, confirm;
 	public static int CLEAR = -2, STOP_CODE = -1, INFINITE = 0;
 	
@@ -14,13 +14,12 @@ public class MotorPacket extends Packet {
 	
 	/**
 	 * Initializes the packet to set the device at port to value.
-	 * @param device Device to set
+	 * 
 	 * @param port Port that the device is at.
 	 * @param value Value to set the device to.
 	 * @param direction (Primarily for motors) True should be forward.
 	 */
-	public MotorPacket(int device, int port, int value, boolean direction, boolean doConfirm) {
-		this.device = device;
+	public MotorPacket(int port, int value, boolean direction, boolean doConfirm) {
 		this.port = port;
 		this.value = value;
 		forward = direction;
@@ -29,8 +28,7 @@ public class MotorPacket extends Packet {
 
 	@Override
 	public void send(DataOutputStream out) throws IOException {
-		out.write(Packet.getEncodedDevicePort(device, port));
-		out.writeInt(value);
+		out.write(Packet.getEncodedDevicePort(Packet.MOTOR, port));
 		out.writeBoolean(forward);
 		out.writeBoolean(confirm);
 	}
@@ -38,7 +36,6 @@ public class MotorPacket extends Packet {
 	@Override
 	public void recieve(DataInputStream in) throws IOException {
 		int encodedDevice = in.read();
-		device = Packet.decodeDevice(encodedDevice);
 		port = Packet.decodePort(encodedDevice);
 		value = in.readInt();
 		forward = in.readBoolean();
@@ -47,7 +44,7 @@ public class MotorPacket extends Packet {
 	
 	@Override
 	public int getDevicePortCode() {
-		return Packet.getEncodedDevicePort(device, port);
+		return Packet.getEncodedDevicePort(Packet.MOTOR, port);
 	}
 
 }
