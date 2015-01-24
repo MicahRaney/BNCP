38a9d57c239d63a115cb10f7facd303c64c12b4f
Micah Raney <MicahRaney@users.noreply.github.com>
2015-01-24 23:03:03 +0530

    If the user manually resets the connection (by pressing a button), all the motors will now be stopped. Cleared up irrelevant console messages.

--- BNCP NXT/src/bncp/nxt/ServerStart.java
+++ BNCP NXT/src/bncp/nxt/ServerStart.java
@@ -1,27 +1,31 @@
 package bncp.nxt;
+
 import bncp.nxt.comm.BasicPacketHandler;
 import bncp.nxt.comm.USBConnectionServer;
 import lejos.nxt.Button;
 import lejos.nxt.LCD;
 import lejos.nxt.Sound;
 
-
 public class ServerStart {
 
 	/**
 	 * @param args
-	 * @throws InterruptedException 
+	 * @throws InterruptedException
 	 */
-	public static void main(String[] args)  {
-		while(true){
-			USBConnectionServer server = new USBConnectionServer(new BasicPacketHandler());
+	public static void main(String[] args) {
+		while (true) {
+			USBConnectionServer server = new USBConnectionServer(
+					new BasicPacketHandler());
 			server.init();
 			server.start();
-			System.out.println("main done...");
 			Button.waitForAnyPress();
 			Sound.buzz();
 			server.interrupt();
 			LCD.clear();
+			System.out.println("Resetting...");
+			lejos.nxt.Motor.A.stop();// stop all motors if user initiates manual
+			lejos.nxt.Motor.B.stop();//reset
+			lejos.nxt.Motor.C.stop();
 			System.out.println("Restarting...");
 			Sound.beep();
 		}
