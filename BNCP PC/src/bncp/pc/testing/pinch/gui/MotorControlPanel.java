74b3d02645cda5b329e83b1b930490df5768e585
Micah Raney <MicahRaney@users.noreply.github.com>
2015-02-18 21:55:23 +0530

    Incorperated use of Lang instead of hard-coded strings.

--- BNCP PC/src/bncp/pc/testing/pinch/gui/MotorControlPanel.java
+++ BNCP PC/src/bncp/pc/testing/pinch/gui/MotorControlPanel.java
@@ -31,6 +31,7 @@ public class MotorControlPanel extends JPanel implements ChangeListener {
 		accelSlider.setMinorTickSpacing(500);
 		accelSlider.setValue(6000);
 		accelSlider.addChangeListener(this);
+		accelSlider.setToolTipText(Lang.getValue("AccelerationSlider.Tooltip"));
 
 		speedSlider = new JSlider(JSlider.VERTICAL);
 		speedSlider.setName("speed-slider");
@@ -41,17 +42,24 @@ public class MotorControlPanel extends JPanel implements ChangeListener {
 		speedSlider.setMinorTickSpacing(500);
 		speedSlider.setValue(360);
 		speedSlider.addChangeListener(this);
+		speedSlider.setToolTipText(Lang.getValue("SpeedSlider.Tooltip"));
 
 		// initialize the two sub-panels. One for the acceleration slider
 		// and one for the speed slider
 		JPanel subAccel = new JPanel(), subSpeed = new JPanel();
 
 		subAccel.setLayout(new BorderLayout());
-		subAccel.add(new JLabel(Lang.ACCEL_SLIDER_LABEL), BorderLayout.NORTH);
+		subAccel.add(
+				// add a new JLabel above the slider
+				new JLabel(Lang.getValue("AccelerationSlider.Label")),
+				BorderLayout.NORTH);
 		subAccel.add(accelSlider, BorderLayout.CENTER);
 
 		subSpeed.setLayout(new BorderLayout());
-		subSpeed.add(new JLabel(Lang.SPEED_SLIDER_LABEL), BorderLayout.NORTH);
+		subSpeed.add(
+				// add a new JLabel above the sliders
+				new JLabel(Lang.getValue("SpeedSlider.Label")),
+				BorderLayout.NORTH);
 		subSpeed.add(speedSlider, BorderLayout.CENTER);
 
 		// initialize this Panel
@@ -64,15 +72,15 @@ public class MotorControlPanel extends JPanel implements ChangeListener {
 	public void addMotor(EmulatedMotor mot) throws IOException {
 		mot.setSpeed(speedSlider.getValue());
 		mot.setAcceleration(accelSlider.getValue());
-		synchronized(motors){
-			motors.add(mot);	
+		synchronized (motors) {
+			motors.add(mot);
 		}
 	}
 
 	public boolean removeMotor(EmulatedMotor mot) {
 		boolean ret;
-		synchronized(motors){
-			ret = motors.remove(mot);	
+		synchronized (motors) {
+			ret = motors.remove(mot);
 		}
 		return ret;
 	}
@@ -86,42 +94,40 @@ public class MotorControlPanel extends JPanel implements ChangeListener {
 	 */
 	public void stateChanged(ChangeEvent evt) {
 
+		if (evt.getSource() == speedSlider) {// change the speed.
 
-		if(evt.getSource() == speedSlider){//change the speed.
-			
-			int speed = speedSlider.getValue();//get the new speed
-			
-			synchronized(motors){
-				for(EmulatedMotor mot : motors)
+			int speed = speedSlider.getValue();// get the new speed
+
+			synchronized (motors) {
+				for (EmulatedMotor mot : motors)
 					try {
 						mot.setSpeed(speed);
 					} catch (IOException e) {
-						System.out.println("Uh-Oh! Setting the speed on a motor failed! (in MotorControlPanel)");
+						System.out
+								.println("Uh-Oh! Setting the speed on a motor failed! (in MotorControlPanel)");
 						e.printStackTrace();
 					}
 			}
 
-			
-		}
-		else if(evt.getSource() == accelSlider){//change the acceleration
-			
-			int accel = accelSlider.getValue();//get the new acceleration
-			
-			synchronized(motors){
-				for(EmulatedMotor mot : motors)
+		} else if (evt.getSource() == accelSlider) {// change the acceleration
+
+			int accel = accelSlider.getValue();// get the new acceleration
+
+			synchronized (motors) {
+				for (EmulatedMotor mot : motors)
 					try {
 						mot.setAcceleration(accel);
 					} catch (IOException e) {
-						System.out.println("Uh-Oh! Setting the acceleration on a motor failed! (in MotorControlPanel)");
+						System.out
+								.println("Uh-Oh! Setting the acceleration on a motor failed! (in MotorControlPanel)");
 						e.printStackTrace();
 					}
 			}
 
+		} else {// It wasn't the speed slider or the acceleration slider!
+			System.out
+					.println("MotorControlPanel: Got a ChangeEvent that wasn't from one of our sliders...");
 		}
-		else{//It wasn't the speed slider or the acceleration slider!
-			System.out.println("MotorControlPanel: Got a ChangeEvent that wasn't from one of our sliders...");
-		}
-		
 
 	}
 
